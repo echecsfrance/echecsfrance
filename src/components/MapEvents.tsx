c0efb746c9813cd94e815b02578ca39ae77b38e0
Timothy Armes <tim@timothyarmes.com>
2024-04-11 09:05:05 +0200

    Zone collection + creation
    Zone create UI flow

--- src/components/MapEvents.tsx
+++ src/components/MapEvents.tsx
@@ -6,17 +6,27 @@ import { useMapEvent } from "react-leaflet";
 
 import { mapBoundsAtom } from "@/atoms";
 
-const MapEvents = () => {
+const worldBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
+const franceBounds = L.latLngBounds(
+  L.latLng(42.08, -5.12),
+  L.latLng(51.17, 9.53),
+);
+
+type MapEventsProps = {
+  bounds?: L.LatLngBounds;
+  updateMapBoundsAtom?: boolean;
+};
+
+export const MapEvents = ({
+  bounds = franceBounds,
+  updateMapBoundsAtom = false,
+}: MapEventsProps) => {
   const setMapBounds = useSetAtom(mapBoundsAtom);
   const [isPending, startTransition] = useTransition();
 
-  const worldBounds = L.latLngBounds(L.latLng(-90, -180), L.latLng(90, 180));
-  const franceBounds = L.latLngBounds(
-    L.latLng(42.08, -5.12),
-    L.latLng(51.17, 9.53),
-  );
-
   const map = useMapEvent("moveend", () => {
+    if (!updateMapBoundsAtom) return;
+
     // Set the map bounds atoms when the user pans/zooms
     startTransition(() => {
       setMapBounds(map.getBounds());
@@ -25,12 +35,11 @@ const MapEvents = () => {
 
   // Viewport agnostic centering of France & max world bounds
   useEffect(() => {
-    map.setView(franceBounds.getCenter(), map.getBoundsZoom(franceBounds));
+    const zoom = map.getBoundsZoom(bounds);
+    map.setView(bounds.getCenter(), zoom === Infinity ? 10 : zoom);
     map.setMaxBounds(worldBounds);
     map.options.maxBoundsViscosity = 1.0; // Prevents going past bounds while dragging
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
   return null;
 };
-
-export default MapEvents;
